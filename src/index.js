f29886056cdb5f921d61206bdd5e2753bd7bdcac
Santiago Velosa <santiagovelosa@gmail.com>
2021-11-30 17:10:02 -0500

    Fix linters errors

--- src/index.js
+++ src/index.js
@@ -1,45 +1,43 @@
 // IMPORTS
-import _, { forEach } from 'lodash';
-import printMe from './print.js';
 import './style.css';
 
-//ELEMENTS
+// ELEMENTS
 const list = document.querySelector('.list');
 const button = document.createElement('button');
-button.innerHTML = `Clear all completed`;
+button.innerHTML = 'Clear all completed';
 
 // ARRAY OF OBJECTS
-let toDoTasks = [
+const toDoTasks = [
   {
-  description: 'wash the dishes',
-  completed: Boolean,
-  index: 0
-},
-{
-  description: 'complete To Do List project',
-  completed: Boolean,
-  index: 1
-},
-{
-  description: 'fix car',
-  completed: Boolean,
-  index: 2
-},
-]
+    description: 'wash the dishes',
+    completed: Boolean,
+    index: 0,
+  },
+  {
+    description: 'complete To Do List project',
+    completed: Boolean,
+    index: 1,
+  },
+  {
+    description: 'fix car',
+    completed: Boolean,
+    index: 2,
+  },
+];
 
 // FUNCTIONS
-let populateList = function (array) {
-  let result = [];
-  array.forEach(element => {
-    let tagText = `<input type="checkbox"><label>${element.description}</label>`;
+const populateList = function (array) {
+  const result = [];
+  array.forEach((element) => {
+    const tagText = `<input type="checkbox"><label>${element.description}</label>`;
     result.push(tagText);
   });
-  result.forEach(element => {
-    let li = document.createElement('li');
+  result.forEach((element) => {
+    const li = document.createElement('li');
     li.innerHTML = element;
     list.appendChild(li);
   });
-}
+};
 
 populateList(toDoTasks);
 list.appendChild(button);
